c8d62cf0b2aa375a2648cc9659b2f80beab03b3c
min je Jeon <84283712+Jeonminje@users.noreply.github.com>
2023-01-29 16:43:15 +0900

    init settings

---
+++ movieservice/src/main/java/study/movieservice/MovieserviceApplication.java
@@ -0,0 +1,13 @@
+package study.movieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieserviceApplication.class, args);
+	}
+
+}
