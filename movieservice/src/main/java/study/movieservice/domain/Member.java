71b21d294739a348239afc132ffae46951c2c8cf
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2023-01-31 16:04:26 +0900

    Revert "Revert "Add files via upload""
    This reverts commit 326c35cb41f5f88b291846bfb08e141bc32e721f.

--- movieservice/src/main/java/study/movieservice/domain/Member.java
+++ movieservice/src/main/java/study/movieservice/domain/Member.java
@@ -1,17 +1,14 @@
 package study.movieservice.domain;
 
-
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import static study.movieservice.domain.Grade.BASIC;
 
 
 @Getter
-@Setter
+@NoArgsConstructor
 public class Member {
-
-
     private Long id;
     private String loginId;
     private String email;
@@ -19,14 +16,15 @@ public class Member {
     private String nickname;
     private Grade grade;
 
-    public Member(){
-    }
-
     public Member(String loginId, String loginPassword, String email, String nickname) {
         this.loginId = loginId;
         this.loginPassword = loginPassword;
         this.email = email;
         this.nickname = nickname;
         this.grade= BASIC;
+    }
+
+    public void setLoginPassword(String password){
+        this.loginPassword=password;
     }
 }
--- movieservice/src/main/java/study/movieservice/repository/mybatis/MybatisMemberRepository.java
+++ movieservice/src/main/java/study/movieservice/repository/mybatis/MybatisMemberRepository.java
@@ -3,20 +3,17 @@ package study.movieservice.repository.mybatis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.movieservice.domain.Member;
-import study.movieservice.repository.MemberRepository;
 
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class MybatisMemberRepository implements MemberRepository {
-
+public class MybatisMemberRepository implements MemberMapper {
     private final MemberMapper memberMapper;
 
     @Override
-    public Member save(Member member) {
+    public void save(Member member) {
         memberMapper.save(member);
-        return member;
     }
 
     @Override
