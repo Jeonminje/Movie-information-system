b73a40065b035b67b320aa7a2455e12aa9be92a1
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2023-01-31 13:03:46 +0900

    Add files via upload

--- movieservice/src/main/java/study/movieservice/controller/MemberController.java
+++ movieservice/src/main/java/study/movieservice/controller/MemberController.java
@@ -1,32 +1,19 @@
 package study.movieservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import study.movieservice.domain.Member;
 import study.movieservice.service.MemberService;
 
-/*
-가입완료 /member/join
-중복확인 /member/idcheck
-이메일 인증 /member/emailcheck
-캡챠 /member/capcha
-로그인 화면 /login
- */
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
-    @PostMapping("/member/join")
-    @ResponseBody
+    @PostMapping("/join")
     public String join(@RequestBody Member member){
-
         memberService.save(member);
         return "ok";
     }
-
-
 }
--- movieservice/src/main/java/study/movieservice/service/MemberService.java
+++ movieservice/src/main/java/study/movieservice/service/MemberService.java
@@ -1,11 +1,20 @@
 package study.movieservice.service;
 
+import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.movieservice.domain.Grade;
 import study.movieservice.domain.Member;
+import study.movieservice.repository.mybatis.MemberMapper;
 
-public interface MemberService {
+@Service
+@RequiredArgsConstructor
+public class MemberService{
+    private final MemberMapper memberMapper;
 
-    Member save(Member member);
-
-
-
+    public void save(Member member){
+        member.setLoginPassword(BCrypt.hashpw(member.getLoginPassword(),BCrypt.gensalt()));
+        memberMapper.save(member);
+    }
 }
