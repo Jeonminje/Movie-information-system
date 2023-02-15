4c3d4345619725883ca815999a2f5e6418a3e12b
min je Jeon <84283712+Jeonminje@users.noreply.github.com>
2023-02-16 02:39:43 +0900

    SessionConst의 nickname관련 상수 제거

--- src/main/java/study/movieservice/domain/SessionConst.java
+++ src/main/java/study/movieservice/domain/SessionConst.java
@@ -3,7 +3,6 @@ package study.movieservice.domain;
 public class SessionConst {
 
     public static final String MEMBER_ID = "memberId";
-    public static final String NICKNAME = "nickname";
 
     private SessionConst(){}
 }
