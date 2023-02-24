e08fac96cb91855e1485fe9dd608371268ba4ffe
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2023-02-24 10:35:35 +0900

    recommend 기능 구현#22
    if문 내부에 예외상황을 두어 불필요한 else문을 제거하였습니다.

--- src/main/java/study/movieservice/service/RecommendService.java
+++ src/main/java/study/movieservice/service/RecommendService.java
@@ -17,58 +17,52 @@ public class RecommendService {
     /**
      * 추천입력을 받았을때 review 테이블의 해당객체를 등록하는 메소드
      *
-     * @param inputRecommend
-     * 어떤 리뷰인지 알려주는 reviewId와
-     * 1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
-     * @throws IllegalArgumentException
-     * 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
+     * @param inputRecommend 어떤 리뷰인지 알려주는 reviewId와
+     *                       1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
+     * @throws IllegalArgumentException 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
      */
     public void recommendSave(Recommend inputRecommend) {
         Long memberId = sessionManager.getMemberId();
 
-        if (!recommendMapper.findByReviewIdAndMemberId(inputRecommend.getReviewId(), memberId)) {
-            Recommend recommend = Recommend.builder()
-                    .memberId(memberId)
-                    .reviewId(inputRecommend.getReviewId())
-                    .recommendState(inputRecommend.getRecommendState())
-                    .build();
+        if (recommendMapper.findByReviewIdAndMemberId(inputRecommend.getReviewId(), memberId))
+            throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
 
-            recommendMapper.recommendSave(recommend);
-        }
-        else throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
+        Recommend recommend = Recommend.builder()
+                .memberId(memberId)
+                .reviewId(inputRecommend.getReviewId())
+                .recommendState(inputRecommend.getRecommendState())
+                .build();
+
+        recommendMapper.recommendSave(recommend);
     }
 
     /**
      * 추천 수정을 입력받았을때 review 테이블의 해당객체를 수정하는 메소드
      *
-     * @param inputRecommend
-     * 어떤 리뷰인지 알려주는 reviewId와
-     * 1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
-     * @throws IllegalArgumentException
-     * 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
+     * @param inputRecommend 어떤 리뷰인지 알려주는 reviewId와
+     *                       1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
+     * @throws IllegalArgumentException 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
      */
     public void recommendUpdate(Recommend inputRecommend) {
         Long memberId = sessionManager.getMemberId();
 
         if (recommendMapper.findByReviewIdAndMemberId(inputRecommend.getReviewId(), memberId))
-            recommendMapper.recommendUpdate(inputRecommend, memberId);
-        else throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
+            throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
+        recommendMapper.recommendUpdate(inputRecommend, memberId);
     }
 
     /**
      * 추천 삭제를 요청받았을때 review 테이블의 해당객체를 삭제하는 메소드
      *
-     * @param inputRecommend
-     * 어떤 리뷰인지 알려주는 reviewId와
-     * 1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
-     * @throws IllegalArgumentException
-     * 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
+     * @param inputRecommend 어떤 리뷰인지 알려주는 reviewId와
+     *                       1일때 추천, 0일때 비추천을 나타내는 recommendState 정보보가담겨있는 Recommend 객체
+     * @throws IllegalArgumentException 만약 reviewId, memberId가 일치하는 객체가 DB에 없을때 발생
      */
     public void recommendDelete(Recommend inputRecommend) {
         Long memberId = sessionManager.getMemberId();
 
         if (recommendMapper.findByReviewIdAndMemberId(inputRecommend.getReviewId(), memberId))
-            recommendMapper.recommendDelete(memberId, inputRecommend.getReviewId());
-        else throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
+            throw new IllegalArgumentException(FAILED_RECOMMEND_REQUEST.getMessage());
+        recommendMapper.recommendDelete(memberId, inputRecommend.getReviewId());
     }
 }
